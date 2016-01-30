0b20631b5ef5ceb6328dff55ee8200bd43356a01
zBallo <abdallah.elshabrawy@gmail.com>
2016-01-30 16:52:54 -0500

    init push Abdallah, Level scene and bunch of assets

---
+++ Assets/AnimTest.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimTest : MonoBehaviour 
+{
+
+	public void testFunction()
+	{
+	}
+}
---
+++ Assets/Scripts/KnockObjects.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnockObjects : MonoBehaviour 
+{
+	public Vector3 ExpPos;
+	public GameObject[] BrokenObjects;
+	public float radius;
+	public float Power;
+
+	void OnCollisionEnter(Collision Other)
+	{
+		ExpPos = transform.position;
+		if(Other.transform.CompareTag("Player"))
+		{
+			transform.gameObject.SetActive (false);
+
+			//transform.GetComponent<Rigidbody> ().AddExplosionForce (Power, ExpPos, radius);
+
+			for (int x = 0; x < BrokenObjects.Length; x++) 
+			{
+				GameObject tempObject = (GameObject)Instantiate (BrokenObjects [x],transform.position, Quaternion.identity);
+				BrokenObjects [x].transform.GetComponent<Rigidbody> ().AddExplosionForce (Power,tempObject.transform.position , radius);
+			}
+
+
+		}
+	}
+}
