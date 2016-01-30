4958cb4b3128339823af6c1873e3afe067484718
dywallace <dwyllace@gmail.com>
2016-01-30 12:10:26 -0500

    Fixed some organization Added cat swipe

---
+++ Assets/Scripts/Cat/SwiperNoSwiping.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwiperNoSwiping : MonoBehaviour {
+    public GameObject LeftPaw;
+    public GameObject RightPaw;
+
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+            Swipe(false);
+
+        if (Input.GetMouseButtonDown(1))
+            Swipe(true);
+
+    }
+
+    public void Swipe(bool goRight = false)
+    {
+
+    }
+}
